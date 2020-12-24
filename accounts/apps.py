6b25a9749c80d929b04ec42745dd7d27a14dad50
Maksim <mitsuk.maksim@gmail.com>
2020-12-25 01:55:11 +0300

    Отправка токена на почту

--- accounts/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class AccountsConfig(AppConfig):
-    name = 'accounts'
-
-    def ready(self):
-        """Регистрируем сигнал"""
-        import accounts.signal
